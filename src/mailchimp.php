797326a485407d543641b860dfeadeae85694256
Gustavo Straube <gustavo@creativeduo.com.br>
2015-01-31 10:51:31 -0500

    Sample Mailchimp form x API integration.

---
+++ src/mailchimp.php
@@ -0,0 +1,45 @@
+<?php
+
+// See the README to more information about the config.
+
+chdir(dirname(__DIR__));
+require 'vendor/autoload.php';
+
+define('MAILCHIMP_API_KEY',  ''); // Your Mailchimp API key
+define('MAILCHIMP_LIST_ID',  ''); // Your list ID
+define('MAILCHIMP_CITY_TAG', ''); // Your custom field tag
+
+$form = $_POST;
+
+if (!empty($form['email']) && !empty($form['city'])) {
+    $mailchimp = new Mailchimp(MAILCHIMP_API_KEY);
+    $lists = new Mailchimp_Lists($mailchimp);
+    try {
+        $email = [
+            'email' => $form['email'],
+        ];
+        $merge = [
+            MAILCHIMP_CITY_TAG => $form['city'],
+        ];
+        $lists->subscribe(
+            MAILCHIMP_LIST_ID, // List ID
+            $email,            // Subscriber ID, his/her email
+            $merge,            // Custom fields
+            'html',            // E-mail type
+            false              // Confirm subscription by email (double opt-in)?
+        );
+        echo 'Successful subscribed!';
+    } catch (Mailchimp_List_AlreadySubscribed $e) {
+        echo 'Already subscribed.';
+    } catch (Mailchimp_Email_AlreadySubscribed $e) {
+        echo 'Already subscribed.';
+    } catch (Mailchimp_Email_NotExists $e) {
+        echo 'Email not exists.';
+    } catch (Mailchimp_Invalid_Email $e) {
+        echo 'Invalid email.';
+    } catch (Mailchimp_List_InvalidImport $e) {
+        echo 'Invalid data, probably your email.';
+    } catch (Exception $e) {
+        echo $e->getMessage(); // Do not show it to the user
+    }
+}
---
+++ src/newsletter.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Newsletter</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+    <form action="mailchimp.php" method="post">
+        <h1>Newsletter</h1>
+        <label>Email</label>
+        <input type="email" name="email"><br>
+        <label>City</label>
+        <input type="text" name="city"><br>
+        <button type="submit">Subscribe!</button>
+    </form>
+</body>
+</html>
